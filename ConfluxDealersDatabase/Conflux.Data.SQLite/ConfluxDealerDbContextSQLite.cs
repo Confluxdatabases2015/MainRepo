777780f95f687491c882e8103d05ccbc21838a56
danielpetrov <danielpetrov666@mail.bg>
2015-10-20 11:22:39 +0300

    SQLite first steps

---
+++ ConfluxDealersDatabase/Conflux.Data.SQLite/ConfluxDealerDbContextSQLite.cs
@@ -0,0 +1,15 @@
+﻿namespace Conflux.Data.SQLite
+{
+    using System.Data.Entity;
+    using ConfluxDealer.Models.SQLite;
+    
+    public class ConfluxDealerDbContextSQLite : DbContext
+    {
+        public ConfluxDealerDbContextSQLite()
+            : base("ConfluxDealerSQLiteConnectionString")
+        {
+        }
+
+        public IDbSet<Shop> Shops { get; set; }
+    }
+}
